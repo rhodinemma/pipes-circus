b8b1a2ba47487f3fb3c7e736f249f0007f63bef8
rhodinemma <rhodinemma10@gmail.com>
2023-03-03 14:15:01 +0300

    chore: add base files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-circus';
+  name: string = '';
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
