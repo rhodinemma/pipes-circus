5203ed6ea2877238d851928f24c66e059cc47ee1
rhodinemma <rhodinemma10@gmail.com>
2023-03-03 16:54:01 +0300

    feat: custom pipe arguments

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,10 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: number, ...args: unknown[]): unknown {
+  transform(value: any, targetUnits: string): any {
     if (!value) {
       return '';
     }
+
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error('Target unit not supported');
+    }
+
     // convert miles to kilometers
     return value * 1.60934;
   }
